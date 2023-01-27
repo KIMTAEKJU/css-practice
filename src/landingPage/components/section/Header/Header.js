8ad13feb1c6ebb7e30467113513d7dd956fe0afc
KIMTAEKJU <gkffntm12@naver.com>
2023-01-27 21:47:55 +0900

    feat: 이미지 최적화 진행중
    - avif, webp 확장자 파일 제공
    - picture, source 태그를 활용하여 위 이미지 확장자 파일 제공
    - srcSet을 활용하여 브라우저 크기에 따른 이미지 사이즈 변경 처리

--- src/landingPage/components/section/Header/Header.js
+++ src/landingPage/components/section/Header/Header.js
@@ -1,4 +1,13 @@
 import React from 'react';
+
+import DesktopWebp from '../../../assets/PC.webp';
+import TabletWebp from '../../../assets/Tablet.webp';
+import MobileWebp from '../../../assets/Mobile.webp';
+
+import DesktopAvif from '../../../assets/PC.avif';
+import TabletAvif from '../../../assets/Tablet.avif';
+import MobileAvif from '../../../assets/Mobile.avif';
+
 import DesktopImg from '../../../assets/PC.png';
 import MobileImg from '../../../assets/Mobile.png';
 import TabletImg from '../../../assets/Tablet.png';
@@ -8,11 +17,25 @@ import './Header.scss';
 function Header() {
 
     return (
-        <section className='header'>
-            <img className='header__desktop-img' src={DesktopImg}/>
-            <img className='header__tablet-img' src={TabletImg}/>
-            <img className='header__mobile-img' src={MobileImg}/>
-        </section>
+        <div className='banner'>
+            <picture>
+                <source 
+                    className='banner__img' 
+                    srcSet={`${MobileAvif} 370w, ${TabletAvif} 768w, ${DesktopAvif} 1200w`}
+                    type="image/avif"
+                />
+                <source
+                    type="image/webp"
+                    className='banner__img' 
+                    srcSet={`${MobileWebp} 370w, ${TabletWebp} 768w, ${DesktopWebp} 1200w`}
+                />
+                <img 
+                    className='banner__img' 
+                    src={DesktopImg}
+                    srcSet={`${MobileImg} 370w ${TabletImg} 768w ${DesktopImg} 1200w`}
+                />
+            </picture>
+        </div>
     )
 }
 
